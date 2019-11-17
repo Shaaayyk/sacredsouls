2ead688f229aad340b5e25424a1022238214d2db
Cordero <ChristopherCordero29@gmail.com>
2019-11-17 11:20:27 -0500

    main page complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import React from "react";
+import "./App.css";
+// import { Route } from 'react-router-dom';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainPage from "./components/MainPage";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
-    <Footer />
+      <MainPage />
+      <Footer />
     </div>
   );
 }
