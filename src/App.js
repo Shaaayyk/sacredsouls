e5c1dff2e4061f67df4891f04555fc364e124008
Cordero <ChristopherCordero29@gmail.com>
2019-11-21 10:50:43 -0500

    Christopher Cordero

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainPage from "./components/MainPage";
 import Schedule from "./components/Schedule";
+import Hamburger from "./components/Hamburger";
 
 function App() {
   return (
     <div className="app">
+      <Hamburger />
       <Header />
       <Route exact path='/' render={() => (
         <MainPage />
